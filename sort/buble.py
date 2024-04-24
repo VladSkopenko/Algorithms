efc8aa393aac0782d8665539d041c89b63642f37
VladSkopenko <skopirka2k17@gmail.com>
2024-04-24 18:38:54 +0300

    fix bubblesort

--- sort/buble.py
+++ sort/buble.py
@@ -2,7 +2,7 @@ import random
 
 from time_decor import timer
 
-lst = [random.randint(1, 10) for i in range(5000)]
+lst = [random.randint(1, 10) for i in range(10000)]
 
 
 @timer
@@ -16,14 +16,11 @@ def bubble_sort(arr):
     :param arr: list
     :return: list
     """
-    length_of_list = len(arr)
-    for index in range(length_of_list - 1):
-        if arr[index] >= arr[index + 1]:
-            arr[index], arr[index + 1] = arr[index + 1], arr[index]
-
-        for index_ in range(length_of_list - 1):
-            if arr[index_] >= arr[index_ + 1]:
-                arr[index_], arr[index_ + 1] = arr[index_ + 1], arr[index_]
+    length_of_list = len(arr) - 1
+    for _ in range(length_of_list):
+        for index in range(length_of_list):
+            if arr[index] >= arr[index + 1]:
+                arr[index], arr[index + 1] = arr[index + 1], arr[index]
     return print(arr)
 
 
