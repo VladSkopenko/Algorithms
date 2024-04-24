61a7c63d6508257b5de5ca97f7343f3ad7692ad2
VladSkopenko <skopirka2k17@gmail.com>
2024-04-24 19:13:26 +0300

    optimized the bubble

--- sort/buble.py
+++ sort/buble.py
@@ -2,7 +2,7 @@ import random
 
 from time_decor import timer
 
-lst = [random.randint(1, 10) for i in range(10000)]
+lst = [random.randint(1, 10) for i in range(50000)]
 
 
 @timer
@@ -17,8 +17,8 @@ def bubble_sort(arr):
     :return: list
     """
     length_of_list = len(arr) - 1
-    for _ in range(length_of_list):
-        for index in range(length_of_list):
+    for n_iter in range(length_of_list):
+        for index in range(length_of_list - n_iter):
             if arr[index] >= arr[index + 1]:
                 arr[index], arr[index + 1] = arr[index + 1], arr[index]
     return print(arr)
