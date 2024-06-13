22e2ece16e427bfa8293288472cec86d1da72d0e
VladSkopenko <skopirka2k17@gmail.com>
2024-06-13 14:41:08 +0300

    add stack and base queues

--- data_structures/queues.py
+++ data_structures/queues.py
@@ -1,4 +1,18 @@
 from queue import Queue
 
+q = Queue(maxsize=3)
 
+q.put("a")
+q.put("b")
+print(q.full())
+q.put("c")
+print(q.full())
+print(q.queue)
+q.get()
+print(q.queue)
+print(q.empty())
 
+"""
+Очередь (queue) в Python - это структура данных, которая работает по принципу "первым пришел - первым вышел" 
+(FIFO, First-In-First-Out). Это означает, что элементы добавляются в конец очереди и извлекаются из начала.
+"""
