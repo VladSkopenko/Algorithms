1d1e7e3f411a19539a0ebb5095a6fdfee0b89df0
VladSkopenko <skopirka2k17@gmail.com>
2024-06-21 13:08:06 +0300

    insert end and start

--- data_structures/linked_list.py
+++ data_structures/linked_list.py
@@ -8,3 +8,16 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
+    def insert_at_end(self, data):
+        if not self.head:
+            self.head = Node(data)
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = Node(data)
+
+    def insert_at_beginning(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
