44dd47b2e9786b59c6ab8b1cf09a7c1aece96c46
VladSkopenko <skopirka2k17@gmail.com>
2024-06-21 13:12:41 +0300

    linked search

--- data_structures/linked_list.py
+++ data_structures/linked_list.py
@@ -21,3 +21,19 @@ class LinkedList:
         new_node = Node(data)
         new_node.next = self.head
         self.head = new_node
+
+    def insert_after(self, prev_node: Node, data):
+        if not prev_node:
+            print("Previous node doesn't exist")
+            return
+        new_node = Node(data)
+        new_node.next = prev_node.next
+        prev_node.next = new_node
+
+    def search(self, data):
+        current = self.head
+        while current:
+            if current.data == data:
+                return current
+            current = current.next
+        return None
