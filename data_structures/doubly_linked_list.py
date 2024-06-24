06b1b4880716d64496d49b96899056cbfaba4f01
VladSkopenko <skopirka2k17@gmail.com>
2024-06-24 16:17:33 +0300

    append double linked list

--- data_structures/doubly_linked_list.py
+++ data_structures/doubly_linked_list.py
@@ -10,3 +10,14 @@ class DoublyLinkedList:
         self.head = None
         self.tail = None
 
+    def push(self,data):
+        new_node = Node(data)
+        new_node.next = self.head
+        if self.head:
+            self.head.prev = new_node
+        else:
+            self.tail = new_node
+        self.head = new_node
+
+
+
