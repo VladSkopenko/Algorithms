5fd1407b913e5eaeefbd068d3a32e6b0b4997cc6
VladSkopenko <skopirka2k17@gmail.com>
2024-06-30 13:23:18 +0300

    update dublelinkedlist insert after

--- data_structures/doubly_linked_list.py
+++ data_structures/doubly_linked_list.py
@@ -19,5 +19,18 @@ class DoublyLinkedList:
             self.tail = new_node
         self.head = new_node
 
+    def insert_after(self, prev_node, data):
+        if not prev_node:
+            print("Previous node doesn't exist")
+            return
+        new_node = Node(data)
+        new_node.next = prev_node.next
+        prev_node.next = new_node
+        new_node.prev = prev_node
+        if new_node.next:
+            new_node.next.prev = new_node
+        else:
+            self.tail = new_node
+
 
 
