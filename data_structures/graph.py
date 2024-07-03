81c1da0ecdcea2158c4a4ebbac52d3d483b4ab60
VladSkopenko <skopirka2k17@gmail.com>
2024-07-03 18:10:14 +0300

    create grapsh and npde for grapth

---
+++ data_structures/graph.py
@@ -0,0 +1,20 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+graph = nx.Graph()   # створення графа
+
+
+graph.add_node("Vlad")
+graph.add_node("Lena")
+graph.add_node("OLeg")
+graph.add_node("Luida")
+graph.add_node("Alica")
+graph.add_node("Pusha")
+
+
+
+
+
+
+
+
