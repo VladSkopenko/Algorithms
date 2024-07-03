e927d934dcb03302bb05ea898bd000c4a7a5aef6
VladSkopenko <skopirka2k17@gmail.com>
2024-07-03 18:12:51 +0300

    add graph

--- data_structures/graph.py
+++ data_structures/graph.py
@@ -11,6 +11,14 @@ graph.add_node("Luida")
 graph.add_node("Alica")
 graph.add_node("Pusha")
 
+graph.add_edge("Vlad", "Lena")
+graph.add_edge("OLeg", "Luida")
+graph.add_edge("Vlad", "Luida")
+graph.add_edge("Alica", "Pusha")
+graph.add_edge("Luida", "Pusha")
+
+nx.draw(graph, with_labels=True)
+plt.show()
 
 
 
