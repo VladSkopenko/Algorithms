5ac5f7c7cb458f1e29a72c9656d3941782d47d78
VladSkopenko <skopirka2k17@gmail.com>
2024-07-21 14:46:22 +0300

    dataclass is sooo cool!!!!

--- feature/dataclasses_my.py
+++ feature/dataclasses_my.py
@@ -1,14 +1,19 @@
 from dataclasses import dataclass
+from abc import ABC
 
 
 @dataclass
-class User:
-    """Дата классы отличная идея  что бы хранить данные так как есть встроенные __eq__ __str__ __repr__ __init__ __new__"""
+class User(ABC):
+    """Дата классы отличная идея  что бы хранить данные так как есть встроенные __eq__ __str__ __repr__ __init__
+    __new__"""
     name: str
     age: int
 
 
+class Buyer(User):
+    pass
+
+
 alex = User('Alex', 23)
 oleg = User("Oleg", 23)
-print(alex.age == oleg.age)
-
+b_alex = Buyer('Alex', 23)
