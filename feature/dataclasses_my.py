7046c9a31c0cb1ce013421abe0f36d377cd54f41
VladSkopenko <skopirka2k17@gmail.com>
2024-07-21 14:53:26 +0300

    dataclasses fisha :

--- feature/dataclasses_my.py
+++ feature/dataclasses_my.py
@@ -1,19 +1,28 @@
-from dataclasses import dataclass
 from abc import ABC
+from dataclasses import dataclass
 
 
-@dataclass
+@dataclass(frozen=False)
 class User(ABC):
     """Дата классы отличная идея  что бы хранить данные так как есть встроенные __eq__ __str__ __repr__ __init__
     __new__"""
     name: str
     age: int
 
+    def get_info(self):
+        ...
 
+
+# @dataclass # Стоит добавить опять если надо пересоздать методы , если становится больше полей, иначе можно не добавлять
 class Buyer(User):
-    pass
+    def get_info(self):
+        print("Buyer", self.name, self.age)
 
 
 alex = User('Alex', 23)
 oleg = User("Oleg", 23)
 b_alex = Buyer('Alex', 23)
+alex.name = "ased"
+print(alex == oleg)
+print(alex == b_alex)
+print(b_alex.get_info())
