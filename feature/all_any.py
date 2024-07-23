e73a6bbd9896dd018db56f2d95055c312ee9cc39
VladSkopenko <skopirka2k17@gmail.com>
2024-07-23 07:29:37 +0300

    funk all and any

---
+++ feature/all_any.py
@@ -0,0 +1,11 @@
+list_1 = [False, False, False, False, False, False, False, False, False, False]
+list_2 = [True, True, True, True, True, True, True, True, True, True]
+list_3 = [False, True, False, True, False, True, False, True, False, True]
+
+print(all(list_1))
+print(all(list_2))
+print(all(list_3))
+print('\n')
+print(any(list_1))
+print(any(list_2))
+print(any(list_3))
