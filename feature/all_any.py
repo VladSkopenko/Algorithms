5c84afdfc9e317680dbc0683c2fccbadebfc4449
VladSkopenko <skopirka2k17@gmail.com>
2024-07-24 11:55:24 +0300

    +

--- feature/all_any.py
+++ feature/all_any.py
@@ -1,7 +1,7 @@
 list_1 = [False, False, False, False, False, False, False, False, False, False]
 list_2 = [True, True, True, True, True, True, True, True, True, True]
 list_3 = [False, True, False, True, False, True, False, True, False, True]
-
+print("Функция all возвращает тру если все значения в массиве True")
 print(all(list_1))
 print(all(list_2))
 print(all(list_3))
